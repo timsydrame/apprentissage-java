9d1a38dcbfc452236779742419ec011e7bafda67
timsydrame <fatimdrame111@gmail.com>
2022-09-25 19:47:55 +0200

    ajout constructeur + methode toString

--- practice-0/src/com/timsy/practice/Person.java
+++ practice-0/src/com/timsy/practice/Person.java
@@ -2,5 +2,12 @@ package com.timsy.practice;
 
 public class Person {
 	private String name;
-
+	
+	public Person(String name) {
+		this.name = name;
+	}
+    
+	public String toString() {
+		return "Person[" + name + "]";
+	}
 }
--- practice-0/src/com/timsy/practice/Test.java
+++ practice-0/src/com/timsy/practice/Test.java
@@ -5,5 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("this is a test");
 		System.out.println("this is a test 2");
+		Person p = new Person("timlaye");
+		System.out.println(p.toString());
 	}
 }
